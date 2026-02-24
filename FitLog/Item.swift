4feaa808721bfd5130a2e4f7df4f70a1dade9eb6
Anthony Cianfrocco <afc4328@rit.edu>
2026-02-24 10:24:38 -0400

    Initial Commit

---
+++ FitLog/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FitLog
+//
+//  Created by Anthony Cianfrocco on 2/24/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ FitLogTests/FitLogTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitLogTests.swift
+//  FitLogTests
+//
+//  Created by Anthony Cianfrocco on 2/24/26.
+//
+
+import Testing
+@testable import FitLog
+
+struct FitLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
