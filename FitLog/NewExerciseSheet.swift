db3467cbac2998c541b7709a23dd94170d90fcb9
Anthony Cianfrocco <afc4328@rit.edu>
2026-02-25 14:33:36 -0400

    add most exhaustive exercise list and allow more modification of workouts

---
+++ FitLog/NewExerciseSheet.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct NewExerciseSheet: View {
+    @EnvironmentObject var dataVM: DataManager
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var description = ""
+    @State private var muscles = ""
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Exercise Info") {
+                    TextField("Name", text: $name)
+                    TextField("Description", text: $description, axis: .vertical)
+                    TextField("Muscles (comma separated)", text: $muscles)
+                }
+            }
+            .navigationTitle("Add New Exercise")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Save") {
+                        let muscleList = muscles.split(separator: ",").map { $0.trimmingCharacters(in: .whitespaces) }
+                        dataVM.addNewExercise(name: name, description: description, muscles: muscleList)
+                        dismiss()
+                    }
+                    .disabled(name.isEmpty)
+                }
+            }
+        }
+    }
+}
